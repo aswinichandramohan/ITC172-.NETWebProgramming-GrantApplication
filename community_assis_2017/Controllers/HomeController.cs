82a88bfbe378e103ef10f704d5edb5001670fd72
aswinichandramohan <35481342+aswinichandramohan@users.noreply.github.com>
2018-04-26 11:41:04 -0700

    Add project files.

---
+++ community_assis_2017/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using community_assis_2017.Models; // reference to model(project name)
+
+namespace community_assis_2017.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();//CommunityAssist2017Entities is a name of the model(your database name)
+            return View(db.GrantTypes.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ community_assis_2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="community_assis_2017.MvcApplication" Language="C#" %>
